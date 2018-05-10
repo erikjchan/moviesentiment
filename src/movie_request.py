42414d358305bc25136728bbcab867aac5c5e659
ejc233 <ejc233@cornell.edu>
2018-05-10 13:48:43 -0400

    collect info from top movies in now_playing.json and upcoming.json

--- src/movie_request.py
+++ src/movie_request.py
@@ -4,63 +4,76 @@ import urllib3
 
 request_url = 'https://api.themoviedb.org/3/'
 now_playing_url = 'movie/now_playing?language=en-US&page=1&api_key='
+upcoming_url = 'movie/upcoming?language=en-US&page=1&api_key='
 genres_url = 'genre/movie/list?api_key='
 movie_id = 1
 credits_url = 'movie/{movie_id}/credits?api_key='
 details_url = 'movie/{movie_id}?api_key='
 api_key = '06709af3621440ead23fb1f3e554ee3d'
 
-r = requests.get(url= request_url + now_playing_url + api_key);
-playing_json = r.json()
+# Collect information from top 20 playing movies
+now_playing_dict = dict()
+now_playing_req = requests.get(url= request_url + now_playing_url + api_key);
+playing_json = now_playing_req.json()
 
-# {u'poster_path': u'/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
-# u'title': u'Coco', 
-# u'overview': u"Despite his family\u2019s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.", 
-# u'release_date': u'2017-10-27', 
-# u'popularity': 311.848958, 
-# u'original_title': u'Coco', 
-# u'backdrop_path': u'/askg3SMvhqEl4OL52YuvdtY40Yb.jpg', 
-# u'vote_count': 3686, 
-# u'video': False, 
-# u'adult': False, 
-# u'vote_average': 7.8, 
-# u'genre_ids': [12, 35, 10751, 16], 
-# u'id': 354912, 
-# u'original_language': 
-# u'en'}
 for res in playing_json['results']:
-	print ' '
-	movie_id = str(res['id'])
-	r = requests.get(url= request_url + 'movie/' + movie_id + '/credits?api_key=' + api_key);
-	credits_json = r.json()
-	print res['title']
-	for i in range(min(10, len(credits_json['cast']))):
-		print credits_json['cast'][i]['name']
+    movie = dict()
+    movie_id = str(res['id'])
+    movie_details_req = requests.get(url= request_url + 'movie/' + movie_id + '?api_key=' + api_key)
+    movie_details = movie_details_req.json()
+    movie['id'] = movie_id
+    movie['title'] = movie_details['title']
+    movie['releaseDate'] = movie_details['release_date']
+    movie['duration'] = movie_details['runtime']
+    movie['genres'] = movie_details['genres']
+    movie['overview'] = movie_details['overview']
+    movie['homepage'] = movie_details['homepage']
+    movie['poster'] = "https://image.tmdb.org/t/p/w1280" + movie_details['poster_path']
+    movie['budget'] = movie_details['budget']
+    credits_req = requests.get(url= request_url + 'movie/' + movie_id + '/credits?api_key=' + api_key);
+    credits_json = credits_req.json()
+    import time
+    time.sleep(1)
+    cast = []
+    for i in range(min(10, len(credits_json['cast']))):
+      cast = cast + [credits_json['cast'][i]['name']]
+    movie['cast'] = cast
+    now_playing_dict[movie_id] = movie
 
-# {u'poster_path': u'/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg', 
-# u'production_countries': [{u'iso_3166_1': u'US', u'name': u'United States of America'}], 
-# u'revenue': 700920729, 
-# u'overview': u"Despite his family\u2019s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.", 
-# u'video': False, 
-# u'id': 354912, 
-# u'genres': [{u'id': 12, u'name': u'Adventure'}, {u'id': 35, u'name': u'Comedy'}, {u'id': 10751, u'name': u'Family'}, {u'id': 16, u'name': u'Animation'}], 
-# u'title': u'Coco', 
-# u'tagline': u'The celebration of a lifetime', 
-# u'vote_count': 3745, 
-# u'homepage': u'https://www.pixar.com/feature-films/coco', 
-# u'belongs_to_collection': None, 
-# u'original_language': u'en', 
-# u'status': u'Released', 
-# u'spoken_languages': [{u'iso_639_1': u'es', u'name': u'Espa\xf1ol'}],
-# u'imdb_id': u'tt2380307', 
-# u'adult': False, 
-# u'backdrop_path': u'/askg3SMvhqEl4OL52YuvdtY40Yb.jpg', 
-# u'production_companies': [{u'origin_country': u'US', u'logo_path': u'/1TjvGVDMYsj6JBxOAkUHpPEwLf7.png', u'id': 3, u'name': u'Pixar'}], 
-# u'release_date': u'2017-10-27', 
-# u'popularity': 310.848958, 
-# u'original_title': u'Coco', 
-# u'budget': 175000000, 
-# u'vote_average': 7.8, 
-# u'runtime': 105}
-r = requests.get(url= request_url + 'movie/354912?api_key=' + api_key);
-# print r.json()
+with open('now_playing.json', 'w') as outfile:
+    json.dump(now_playing_dict, outfile, sort_keys=True, indent=4)
+
+
+
+# Collect information from top 20 upcoming movies
+upcoming_dict = dict()
+upcoming_req = requests.get(url= request_url + upcoming_url + api_key);
+upcoming_json = upcoming_req.json()
+
+for res in upcoming_json['results']:
+    movie = dict()
+    movie_id = str(res['id'])
+    movie_details_req = requests.get(url= request_url + 'movie/' + movie_id + '?api_key=' + api_key)
+    movie_details = movie_details_req.json()
+    movie['id'] = movie_id
+    movie['title'] = movie_details['title']
+    movie['releaseDate'] = movie_details['release_date']
+    movie['duration'] = movie_details['runtime']
+    movie['genres'] = movie_details['genres']
+    movie['overview'] = movie_details['overview']
+    movie['homepage'] = movie_details['homepage']
+    movie['poster'] = "https://image.tmdb.org/t/p/w1280" + movie_details['poster_path']
+    movie['budget'] = movie_details['budget']
+    credits_req = requests.get(url= request_url + 'movie/' + movie_id + '/credits?api_key=' + api_key);
+    credits_json = credits_req.json()
+    import time
+    time.sleep(1)
+    cast = []
+    for i in range(min(10, len(credits_json['cast']))):
+      cast = cast + [credits_json['cast'][i]['name']]
+    movie['cast'] = cast
+    upcoming_dict[movie_id] = movie
+
+with open('upcoming.json', 'w') as outfile:
+    json.dump(upcoming_dict, outfile, sort_keys=True, indent=4)
+
--- src/twitter/tweet_listener.py
+++ src/twitter/tweet_listener.py
@@ -1,54 +1,41 @@
-import twitter_settings
-import tweepy
-import dataset
 from textblob import TextBlob
 from sqlalchemy.exc import ProgrammingError
+import dataset
 import json
+import settings
+import tweepy
 
-db = dataset.connect(twitter_settings.CONNECTION_STRING)
+db = dataset.connect(settings.CONNECTION_STRING)
 
 class StreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
+        print(status)
         if status.retweeted:
             return
 
         description = status.user.description
-        loc = status.user.location
         text = status.text
-        coords = status.coordinates
-        geo = status.geo
         name = status.user.screen_name
         user_created = status.user.created_at
         followers = status.user.followers_count
         id_str = status.id_str
         created = status.created_at
         retweets = status.retweet_count
-        bg_color = status.user.profile_background_color
         blob = TextBlob(text)
         sent = blob.sentiment
 
-        if geo is not None:
-            geo = json.dumps(geo)
-
-        if coords is not None:
-            coords = json.dumps(coords)
-
-        table = db[twitter_settings.TABLE_NAME]
+        table = db[settings.TABLE_NAME]
         try:
             table.insert(dict(
                 user_description=description,
-                user_location=loc,
-                coordinates=coords,
                 text=text,
-                geo=geo,
                 user_name=name,
                 user_created=user_created,
                 user_followers=followers,
                 id_str=id_str,
                 created=created,
                 retweet_count=retweets,
-                user_bg_color=bg_color,
                 polarity=sent.polarity,
                 subjectivity=sent.subjectivity,
             ))
@@ -60,9 +47,9 @@ class StreamListener(tweepy.StreamListener):
             #returning False in on_data disconnects the stream
             return False
 
-auth = tweepy.OAuthHandler(twitter_settings.TWITTER_APP_KEY, twitter_settings.TWITTER_APP_SECRET)
-auth.set_access_token(twitter_settings.TWITTER_KEY, twitter_settings.TWITTER_SECRET)
+auth = tweepy.OAuthHandler(settings.TWITTER_APP_KEY, settings.TWITTER_APP_SECRET)
+auth.set_access_token(settings.TWITTER_KEY, settings.TWITTER_SECRET)
 api = tweepy.API(auth)
 stream_listener = StreamListener()
 stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
-stream.filter(track=twitter_settings.TRACK_TERMS)
+stream.filter(track=settings.TRACK_TERMS)
